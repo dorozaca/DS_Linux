c99a6949b8a9440d036f4db7046b3992373f3cac
Diego <diego.oroza7@gmail.com>
2023-08-06 09:21:06 -0500

    test

---
+++ testing2.py
@@ -0,0 +1,15 @@
+data = {
+    "hey": 12344,
+    "package": {"a": 5, "b": 100},
+    "True": [1, 2, 3, 4],
+}
+print(data)
+
+
+def even_numbers(i)
+    
+    for i in range(1, 100):
+        if i % 2 == 0:
+            print(i)  # print even numbers
+
+def palindrome
